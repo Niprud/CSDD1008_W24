c2b1777ce6f2d0c99d3189424522a3449f8f690c
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-01-18 19:02:57 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CSDD1008_W24
+
+# How to initaite go package
+go mod init
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Print("Hello World");
+	var number int
+	var text string
+	number = 10
+	text = "hello"
+	fmt.Print(text, number)
+} 
